deae5f1621932726a07a49687f7f36694ec3bcb6
David Ruda <davidrth@seznam.cz>
2023-01-20 02:52:16 +0100

    Add default implementation - 1 sec green for each incoming street (for every intersection)

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # david_ruda
 
 ## [Problem description](https://storage.googleapis.com/coding-competitions.appspot.com/HC/2021/hashcode_2021_online_qualification_round.pdf)  
+[Problem submission and interactive page](https://codingcompetitions.withgoogle.com/hashcode/round/00000000004361e2/0000000000c617e4)  
 [Google Hash Code archive](https://codingcompetitions.withgoogle.com/hashcode/archive)  
 [Kaggle competition with additional data](https://www.kaggle.com/competitions/hashcode-2021-oqr-extension/overview)  
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import os
 
 class Street:
-    def __init__(self, id, start, end, name, length):
+    def __init__(self, id: int, start: int, end: int, name: str, length: int):
         self.id = id
         self.start = start
         self.end = end
@@ -13,13 +13,50 @@ class Street:
         return f'{self.id} {self.name} {self.start} -> {self.end} length: {self.length}'
 
 class Car:
-    def __init__(self, id, path_length, path):
+    def __init__(self, id: int, path_length: int, path: list[str]):
         self.id = id
         self.path_length = path_length
         self.path = path
 
     def __str__(self):
         return f'{self.id} length: {self.path_length} ' + ' '.join(self.path)
+
+class Intersection:
+    def __init__(self, id: int):
+        self.id = id
+        self.incoming = []
+        self.outgoing = []
+
+    def __str__(self):
+        lines = []
+        lines.append(str(self.id))
+        lines.append('Incoming streets:')
+        lines.extend(map(str, self.incoming))
+        lines.append('Outgoing streets:')
+        lines.extend(map(str, self.outgoing))
+        return '\n'.join(lines)
+
+class Simulation:
+    def __init__(
+        self, duration: int, intersections: list[Intersection], 
+        streets: list[Street], cars: list[Car], bonus: int
+        ):
+        self.duration = duration
+        self.intersections = intersections
+        self.streets = streets
+        self.cars = cars
+        self.bonus = bonus
+
+    def __str__(self):
+        lines = []
+        lines.append(f'Duration: {self.duration}')
+        lines.append(f'Number of intersections: {len(self.intersections)}')
+        lines.append(f'Bonus points: {self.bonus}')
+        lines.append(f'Streets: {len(self.streets)}')
+        lines.extend(map(str, self.streets))
+        lines.append(f'Cars: {len(self.cars)}')
+        lines.extend(map(str, self.cars))
+        return '\n'.join(lines)
 
 def read_input(file):
 
@@ -31,29 +68,47 @@ def read_input(file):
     num_of_cars = int(V)
     bonus = int(F)
 
+    intersections = [Intersection(i) for i in range(num_of_intersections)]
+
     lines = file.read().splitlines()
     streets = []
     for i, line in enumerate(lines[:num_of_streets]):
-        streets.append(Street(i, *line.split()))
+        start, end, name, length = line.split()
+        street = Street(i, int(start), int(end), name, int(length))
+        streets.append(street)
+        intersections[street.start].outgoing.append(street)
+        intersections[street.end].incoming.append(street)
 
     cars = []
     for i, line in enumerate(lines[num_of_streets:]):
         tokens = line.split()
-        cars.append(Car(i, tokens[0], tokens[1:]))
+        cars.append(Car(i, int(tokens[0]), tokens[1:]))
 
-    print(f'Duration: {duration}')
-    print(f'Number of intersections: {num_of_intersections}')
-    print(f'Bonus points: {bonus}')
-    print(f'Streets: {num_of_streets}')
-    for street in streets:
-        print(street)
-    print(f'Cars: {num_of_cars}')
-    for car in cars:
-        print(car)
+    return Simulation(duration, intersections, streets, cars, bonus)
 
+def create_output_default(simulation: Simulation):
+    os.makedirs(OUTPUT_FOLDER, exist_ok=True)
+    with open(os.path.join(OUTPUT_FOLDER, FILENAME + EXTENSION_OUT), 'w') as f:
+        f.write(str(len(simulation.intersections)) + '\n')
+        for intersection in simulation.intersections:
+            f.write(str(intersection.id) + '\n')
+            f.write(str(len(intersection.incoming)) + '\n')
+            for incoming in intersection.incoming:
+                f.write(incoming.name + ' 1\n')
+    
 
 if __name__ == '__main__':
     DATA_FOLDER = 'input_data'
-    FILENAME = 'a_an_example.in.txt'
-    with open(os.path.join(DATA_FOLDER, FILENAME)) as f:
-        read_input(f)
+    OUTPUT_FOLDER = 'output'
+    EXTENSION_IN = '.in.txt'
+    EXTENSION_OUT = '.out.txt'
+    FILENAME = 'a_an_example'
+    #FILENAME = 'b_by_the_ocean'
+    #FILENAME = 'c_checkmate'
+    #FILENAME = 'd_daily_commute'
+    #FILENAME = 'e_etoile'
+    #FILENAME = 'f_forever_jammed'
+    with open(os.path.join(DATA_FOLDER, FILENAME + EXTENSION_IN), 'r') as f:
+        simulation = read_input(f)
+    print(simulation)
+    create_output_default(simulation)
