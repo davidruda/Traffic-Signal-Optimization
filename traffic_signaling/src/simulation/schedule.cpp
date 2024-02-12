c32893550eb24c2567f99c1d358112f130ef94ee
David Ruda <davidrth@seznam.cz>
2024-02-12 22:37:02 +0100

    Refactor next_green method

--- traffic_signaling/src/simulation/schedule.cpp
+++ traffic_signaling/src/simulation/schedule.cpp
@@ -45,22 +45,23 @@ void Schedule::add_street(size_t street_id, size_t green_light_duration) {
 }
 
 std::optional<size_t> Schedule::next_green(size_t street_id, size_t time) {
-    if (street_index_.contains(street_id)) {
-        auto &&green_light = green_lights_[street_index_[street_id]];
-        if (green_light.duration() > 0) {
-            auto time_normalized = time % total_duration_;
-            auto start = green_light.begin();
-            if (time_normalized < start) {
-                return (time - time_normalized) + start;
-            }
-            auto end = green_light.end();
-            if (time_normalized >= end) {
-                return (time - time_normalized) + total_duration_ + start;
-            }
-            return time;
-        }
+    if (!street_index_.contains(street_id)) {
+        return {};
     }
-    return {};
+    auto &&green_light = green_lights_[street_index_[street_id]];
+    if (green_light.duration() <= 0) {
+        return {};
+    }
+    auto time_normalized = time % total_duration_;
+    auto start = green_light.begin();
+    if (time_normalized < start) {
+        return (time - time_normalized) + start;
+    }
+    auto end = green_light.end();
+    if (time_normalized >= end) {
+        return (time - time_normalized) + total_duration_ + start;
+    }
+    return time;
 }
 
 std::pair<std::vector<size_t>, std::vector<size_t>> Schedule::get_schedule() const {
